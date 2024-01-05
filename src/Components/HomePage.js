043fb2a177e7536e07b7978ec51e6f4bd80b3237
Hystyic <40578476+Hystyic@users.noreply.github.com>
2024-01-05 14:04:13 +0530

    Update HomePage.js

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,4 +1,5 @@
-import { useAuth0 } from "@auth0/auth0-react"; 
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 const HomePage = () => {
   const { loginWithRedirect } = useAuth0(); 
@@ -16,6 +17,14 @@ const HomePage = () => {
         >
           Login
         </button>
+        {/* Order Button */}
+        <Link to="/meals"> 
+          <button
+            className="flex items-center font-medium rounded-lg text-2xl px-8 py-4 bg-blue-500 hover:bg-blue-600 text-white"
+          >
+            Order Now
+          </button>
+        </Link>
       </div>
     </div>
   );
